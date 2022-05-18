a9118230104271c6c1b63c370a8357afd01b4f45
isaac Azameti <60759789+ikmazameti@users.noreply.github.com>
2022-05-18 12:13:17 +0000

    added bootstrap and defined brand for project

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Route::get('/', function () {
+//    return view('land');
+//});
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
